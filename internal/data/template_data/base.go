73a3654560632028a38a601f41f4caa3e7a22bd0
donbeproud <dontbeproud@foxmail.com>
2024-05-30 15:56:04 +0800

    feat: 模板代码新增调用通用代码的样例

--- internal/data/template_data/base.go
+++ internal/data/template_data/base.go
@@ -12,7 +12,7 @@ type Data struct {
 
 // NewData .
 func NewData(c *template_config.DataSourceCfg, loggerCfg *template_config.LoggerConfig) (*Data, func(), error) {
-	_logger, err := template_config.NewStandardSystemLogger(loggerCfg, "svc", nil, nil, nil)
+	_logger, err := template_config.NewStandardSystemLogger(loggerCfg, "data", nil, nil, nil)
 	cleanup := func() {
 		if _logger != nil {
 			_logger.Info("closing the data resources")
